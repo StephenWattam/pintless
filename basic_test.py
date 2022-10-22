578c25e8941eda031dc56cf997df41170f0fbec7
Steve Wattam <steve@watt.am>
2022-10-22 11:11:01 +0900

    Reformat test script for very little reason

--- basic_test.py
+++ basic_test.py
@@ -1,10 +1,6 @@
-
-
-
 from pintless import Registry
 
 reg = Registry()
-
 
 
 quantity = 10 * reg.meter
@@ -20,7 +16,7 @@ print(f"-> m*s [2]: {ms2}")
 print(f"ms + ms2 = {ms + ms2}")
 
 # Unit only calculations
-meter_hours = reg.m * reg.H   # meter*hours
+meter_hours = reg.m * reg.H  # meter*hours
 mh50 = 50 * meter_hours
 print(f"mh50: {mh50}")
 
@@ -41,13 +37,16 @@ print(f"Cancelling: {distance/distance} and {time/time} and {mh50 / mh50}")
 print(f"Cancelling with conversion: {(10 * reg.km) / (20 * reg.meter)}")
 
 
-
 # Dimensionless numbers
-print("Dimensionless numbers and interacting with floats")
+print("\nDimensionless numbers and interacting with floats")
 a = 45 * reg.dimensionless
 b = 50 * reg.dimensionless
-print(f"Dimensionless number a={a}, b={b}, a+b={a+b}, a*b={a*b}, a/b={a/b}, a*10={a*10}")
-print(f"Dimensionless * unit: {a * reg.hour}, *ratio: {a * reg.Hz}, *product: {a*reg.kWh}")
+print(
+    f"Dimensionless number a={a}, b={b}, a+b={a+b}, a*b={a*b}, a/b={a/b}, a*10={a*10}"
+)
+print(
+    f"Dimensionless * unit: {a * reg.hour}, *ratio: {a * reg.Hz}, *product: {a*reg.kWh}"
+)
 
 
 # product types
@@ -65,4 +64,6 @@ second = start * reg.ms
 third = second * reg.ms
 
 
-import code; code.interact(local=locals())
+import code
+
+code.interact(local=locals())
