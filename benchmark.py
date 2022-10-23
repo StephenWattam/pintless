6139228fcd1a59004af2a1b530e639073ca08477
Steve Wattam <steve@watt.am>
2022-10-23 21:01:32 +0900

    Added benchmark

--- benchmark.py
+++ benchmark.py
@@ -1,49 +1,41 @@
+"""A small script to benchmark the unit library against pint."""
 
-
-# from pint import UnitRegistry
-# ureg = UnitRegistry()
-
-
+from pint import UnitRegistry
 from pintless import Registry
-ureg = Registry()
-
-distance = 24.0 * ureg.meter
-
-assert str(distance) == "24.0 meter"
+from typing import Union
+import time
 
 
-assert distance.magnitude == 24.0
-assert distance.units == ureg.meter
-assert distance.dimensionality == "[length]"
+def run_benchmark(r: Union[UnitRegistry, Registry]) -> float:
+    start = time.time()
+
+    for _ in range(100_000_000):
+
+        # Basic arithmetic
+        length_a = 10 * r.meter
+        length_b = 10 * r.inch
+        result = length_a + length_b * 10
+
+        # Unit arithmetic
+        result = length_a * r.kWh
+        result = r.kWh / r.second
+        result = r.Hz * r.hour
+
+        # Parsing strings
+        result = length_a.to("inch")
+        result = length_b.to("mile")
+        # result = r.get_unit("mile / hour")
+
+        end = time.time()
+        return end - start
 
 
+pintful_reg = UnitRegistry()
+time_pint = run_benchmark(pintful_reg)
+pintless_reg = Registry()
+time_pintless = run_benchmark(pintless_reg)
 
-
-time = 8.0 * ureg.second
-assert str(time) == "8.0 second"
-speed = distance / time
-assert speed.units == ureg.meter / ureg.second
-
-assert speed.magnitude == 3.0
-assert str(speed) == "3.0 meter / second"
-assert str(speed.dimensionality) == "[length] / [time]"
-
-
-# print(f" ===> {speed.to('inch/minute')}")
-# assert speed.to('inch/minute') == 7086.614173228347 * ureg.inch / ureg.minute
-# assert speed.to(ureg.inch / ureg.minute) == speed.to("inch/minute")
-
-
-assert str(speed) == "3.0 meter / second"
-
-# speed.ito("inch/minute")
-# assert str(speed) == "7086.614173228347 inch / minute"
-
-
-
-
-
-
-print(f"------")
-print(f"Stuff completed successfully, here's a REPL")
-import code; code.interact(local=locals())
+print(f"    Time (pint): {time_pint:0.4}s")
+print(f"Time (pintless): {time_pintless:0.4}s")
+print("")
+print(f"Pintless is {time_pint/time_pintless:0.4} times faster")
